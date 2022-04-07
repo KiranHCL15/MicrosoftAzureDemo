72a8f048b77074915993ee921501d1695727fa73
kiran <kiran.ghorpade@hcl.com>
2022-04-07 13:51:45 +0530

    newspace

--- GitProjects/src/Package/Myclass.java
+++ GitProjects/src/Package/Myclass.java
@@ -5,7 +5,7 @@ public class Myclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi");
-		System.out.println("h ello");
+		System.out.println("h e llo");
 
 	}
 
