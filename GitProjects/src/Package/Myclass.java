5b544d306367db402e725c5bf90d1d2dead335e6
kiran <kiran.ghorpade@hcl.com>
2022-04-07 14:00:03 +0530

    master

--- GitProjects/src/Package/Myclass.java
+++ GitProjects/src/Package/Myclass.java
@@ -5,7 +5,7 @@ public class Myclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi");
-		System.out.println("h e llo");
+		System.out.println("h e l lo");
 
 	}
 
